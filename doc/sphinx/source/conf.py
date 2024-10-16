7b881d9651fe6b68548806a428b2128d21f14711
Manuel YGUEL <manuel.yguel@gmail.com>
2024-10-16 07:45:18 +0200

    First commit

---
+++ doc/sphinx/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Apprentissage par Renforcement et Simulation'
+copyright = '2024, Manuel YGUEL'
+author = 'Manuel YGUEL'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+source_suffix = [".rst", ".md"]
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+
+# -- Options for copybutton extension ----------------------------------------
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+html_static_path = ['_static']
+
+
+html_css_files = [
+    "css/custom.css",
+]
---
+++ doc/sphinx/source/index.rst
@@ -0,0 +1,10 @@
+##########################################################
+Apprentissage par Renforcement et Simulation documentation
+##########################################################
+
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
