c9c73a5a4eebc59092be63c1a1cd4b4abb617b4e
Manuel YGUEL <manuel.yguel@gmail.com>
2025-10-22 21:46:25 +0200

    Fix path instructions.

--- doc/sphinx/source/l01_introduction_with_mujoco/l01s01_installation_mujoco.rst
+++ doc/sphinx/source/l01_introduction_with_mujoco/l01s01_installation_mujoco.rst
@@ -4,23 +4,35 @@ Installation d'un environnement MuJoCo
 
 #. Installation en utilisant docker. C'est la plus simple et qui marche sur windows, mac et linux.
 
+Dans la salle de cours, docker est déjà installé sur les machines linux. Si vous êtes sur votre propre machine, suivez les instructions ci-dessous pour installer docker.
+
+.. dropdown:: Comment installer Docker sur votre machine si besoin ?
+
+   .. tab-set::
+
+      .. tab-item:: Installer Docker sur Linux
+
+         Pour installer docker sur linux, vous pouvez suivre les instructions officielles: `https://docs.docker.com/engine/install/ <https://docs.docker.com/engine/install/>`_.
+
+      .. tab-item:: Installer Docker sur Windows
+
+         Pour installer docker sur Windows, vous pouvez suivre les instructions officielles: `https://docs.docker.com/desktop/install/windows-install/ <https://docs.docker.com/desktop/install/windows-install/>`_.
+
+      .. tab-item:: Installer Docker sur Mac
+
+         Pour installer docker sur Mac, vous pouvez suivre les instructions officielles: `https://docs.docker.com/desktop/install/mac-install/ <https://docs.docker.com/desktop/install/mac-install/>`_.
+
+Vous pouvez suivre les instructions ci-dessous en fonction de votre système d'exploitation.
+
 .. tab-set::
 
-   .. tab-item:: Docker pour linux
+   .. tab-item:: Docker pour Linux (machines de la salle de cours)
 
-      #. Créer un dossier pour stocker les fichiers du cours
-
-         .. code-block:: bash
-
-            mkdir -p ~/rl/mujoco
-
-      #. Télécharger le fichier :download:`start.sh <resources/scripts/start.sh>` et placez le dans le dossier `~/rl/mujoco`
-
-      #. Rendez le script start.sh exécutable:
+      #. Créer un dossier pour stocker les fichiers du cours, télécharger le script d'installation et le rendre exécutable:
 
          .. code-block:: bash
 
-            cd ~/rl/mujoco && chmod +x start.sh
+           mkdir -p ~/rl/mujoco && cd ~/rl/mujoco && wget -O start.sh https://raw.githubusercontent.com/yguel/docker_mujoco_rl_101/main/start.sh && chmod +x start.sh
 
       #. Lancez le script start.sh
 
@@ -28,24 +40,177 @@ Installation d'un environnement MuJoCo
 
             cd ~/rl/mujoco && ./start.sh
 
-      #. Dans une autre fenêtre de navigateur, ouvrez l'URL suivante: `http://localhost:8888 <http://localhost:8888>`_. Vous devriez voir une interface jupyter.
+      .. dropdown:: Si vous rencontrez des problèmes
 
-      #. Dans une fenêtre de navigateur, ouvrez l'URL suivante: `http://localhost:6080 <http://localhost:6080>`_. Vous devriez voir une fenêtre NO VNC, vous n'avez plus qu'à cliquer pour vous connecter et vous devriez voir un bureau linux.
+         Si vous rencontrez des problèmes, voyez les paramètres avancés avec la commande help:
+         
+            .. code-block:: bash
 
-      #. Dans cette dernière fenêtre, vous pouvez maintenant ouvrir un terminal et exécuter les vérifications.
+               ./start.sh --help
+
+         Vous pouvez aussi directement lancer le docker avec des options personnalisées, exemple sans accélération matérielle NVIDIA:
+
+            .. code-block:: bash
+
+               docker run -it --rm --name mujoco-student \
+                  --shm-size=4g \
+                  -p 6080:6080 \
+                  -p 8888:8888 \
+                  -v $HOME/rl/mujoco/workspace:/home/student/workspace \
+                  -e HOST_UID=$(id -u) \
+                  -e HOST_GID=$(id -g) \
+                  -e HOST_WORKSPACE_INFO="Persistent: $HOME/rl/mujoco" \
+                  -e USE_TEMP_WORKSPACE="false" \
+                  -e BACKUP_TARGET_DIR="$HOME/rl/docker_snapshots/mujoco_snapshots" \
+                  -e VNC_RESOLUTION=1920x1080 \
+                  -e VNC_DEPTH=24 \
+                  -e VNC_DPI=96 \
+                  -e NOVNC_PORT=6080 \
+                  -e VNC_PORT=5901 \
+                  -e DISPLAY=:1 \
+                  -e LIBGL_ALWAYS_SOFTWARE=1 \
+                  -e MUJOCO_GL=osmesa \
+                  yguel/mujoco-desktop:v1.0
+
+         exemple avec accélération matérielle NVIDIA:
+
+            .. code-block:: bash
+
+               docker run -it --rm --name mujoco-student \
+                  --shm-size=4g \
+                  -p 6080:6080 \
+                  -p 8888:8888 \
+                  -v $HOME/rl/mujoco/workspace:/home/student/workspace \
+                  -e HOST_UID=$(id -u) \
+                  -e HOST_GID=$(id -g) \
+                  -e HOST_WORKSPACE_INFO="Persistent: $HOME/rl/mujoco" \
+                  -e USE_TEMP_WORKSPACE="false" \
+                  -e BACKUP_TARGET_DIR="$HOME/rl/docker_snapshots/mujoco_snapshots" \
+                  -e VNC_RESOLUTION=1920x1080 \
+                  -e VNC_DEPTH=24 \
+                  -e VNC_DPI=96 \
+                  -e NOVNC_PORT=6080 \
+                  -e VNC_PORT=5901 \
+                  --runtime=nvidia \
+                  -e NVIDIA_VISIBLE_DEVICES=all \
+                  -e DISPLAY=:1 \
+                  -e LIBGL_ALWAYS_SOFTWARE=0 \
+                  yguel/mujoco-desktop:v1.0
+
+
+         exemple avec un workspace temporaire (à sauvegarder manuellement à la fin de la session):
+
+            .. code-block:: bash
+
+               docker run -it --rm --name mujoco-student \
+                  --shm-size=4g \
+                  -p 6080:6080 \
+                  -p 8888:8888 \
+                  -v /tmp/rl/mujoco/workspace:/home/student/workspace \
+                  -e HOST_UID=$(id -u) \
+                  -e HOST_GID=$(id -g) \
+                  -e HOST_WORKSPACE_INFO="Temporary: /tmp/rl/mujoco (will have to be backed up on exit)" \
+                  -e USE_TEMP_WORKSPACE="true" \
+                  -e BACKUP_TARGET_DIR="$HOME/rl/docker_snapshots/mujoco_snapshots" \
+                  -e VNC_RESOLUTION=1920x1080 \
+                  -e VNC_DEPTH=24 \
+                  -e VNC_DPI=96 \
+                  -e NOVNC_PORT=6080 \
+                  -e VNC_PORT=5901 \
+                  -e DISPLAY=:1 \
+                  -e LIBGL_ALWAYS_SOFTWARE=1 \
+                  -e MUJOCO_GL=osmesa \
+                  yguel/mujoco-desktop:v1.0
+
+         dans ce cas n'oubliez pas de sauvegarder votre workspace à la fin de la session en utilisant le :download:`script de sauvegarde automatique <resources/scripts/save_rl_env.sh>`:
+
+            .. code-block:: bash
+
+               ./save_rl_env.sh 
+
+   .. tab-item:: Docker pour Windows
+
+      Note importante, ces instructions n'ont pas été testées, elles sont fournies à titre indicatif.
+      Merci de me faire un retour si vous les testez et rencontrez des problèmes ou au contraire si tout fonctionne bien.
+
+      Dans la suite il faut remplacer ``<VotreNom>`` par votre nom d'utilisateur windows.
+
+      #. Avec docker desktop installé, ouvrez une fenêtre powershell et créez un dossier pour stocker les fichiers du cours:
+
+         .. code-block:: bash
+
+            mkdir $HOME\rl\mujoco
+
+      #. Si vous n'avez pas d'accélération matérielle NVIDIA, lancer l'image docker manuellement en liant le dossier créé précédemment, en alouant les ports pour jupyter et noVNC, et alouant suffisamment de mémoire (ici 4Go, mais ne mettez pas plus de la moitié de la mémoire totale de votre machine):
+
+         .. code-block:: bash
+
+            docker run -it --rm --name mujoco-student `
+               --shm-size=4g `
+               -p 6080:6080 `
+               -p 8888:8888 `
+               -v C:\Users\<VotreNom>\rl\mujoco\workspace:/home/student/workspace `
+               -e HOST_WORKSPACE_INFO="Persistent: C:\Users\<VotreNom>\rl\mujoco" `
+               -e USE_TEMP_WORKSPACE="false" `
+               -e BACKUP_TARGET_DIR="C:\Users\<VotreNom>\rl\docker_snapshots\mujoco_snapshots" `
+               -e VNC_RESOLUTION=1920x1080 `
+               -e VNC_DEPTH=24 `
+               -e VNC_DPI=96 `
+               -e NOVNC_PORT=6080 `
+               -e VNC_PORT=5901 `
+               -e DISPLAY=:1 `
+               -e LIBGL_ALWAYS_SOFTWARE=1 `
+               -e MUJOCO_GL=osmesa `
+               yguel/mujoco-desktop:v1.0
+
+      #. Si vous avez une carte graphique NVIDIA compatible avec l'accélération matérielle dans docker, vous pouvez lancer l'image docker avec les options supplémentaires suivantes (à ajouter avant le nom de l'image yguel/mujoco-desktop:v1.0):
+
+         .. code-block:: bash
+
+            docker run -it --rm --name mujoco-student `
+               --shm-size=4g `
+               -p 6080:6080 `
+               -p 8888:8888 `
+               -v C:\Users\<VotreNom>\rl\mujoco\workspace:/home/student/workspace `
+               --runtime=nvidia `
+               -e NVIDIA_VISIBLE_DEVICES=all `
+               -e HOST_WORKSPACE_INFO="Persistent: C:\Users\<VotreNom>\rl\mujoco" `
+               -e USE_TEMP_WORKSPACE="false" `
+               -e BACKUP_TARGET_DIR="C:\Users\<VotreNom>\rl\docker_snapshots\mujoco_snapshots" `
+               -e VNC_RESOLUTION=1920x1080 `
+               -e VNC_DEPTH=24 `
+               -e VNC_DPI=96 `
+               -e NOVNC_PORT=6080 `
+               -e VNC_PORT=5901 `
+               -e DISPLAY=:1 `
+               -e LIBGL_ALWAYS_SOFTWARE=0 `
+               yguel/mujoco-desktop:v1.0
    
 
-========================================
-Première vérification de l'installation
-========================================
+
+==========================================
+Premières vérifications de l'installation
+==========================================
+
+#. Dans une autre fenêtre de navigateur, ouvrez l'URL suivante: `http://localhost:8888 <http://localhost:8888>`_. Vous devriez voir une interface jupyter.
+
+#. Dans une autre fenêtre de navigateur, ouvrez l'URL suivante: `http://localhost:6080 <http://localhost:6080>`_. Vous devriez voir une fenêtre NO VNC, vous n'avez plus qu'à cliquer pour vous connecter et vous devriez voir un bureau linux comme sur la :numref:`fig_docker_ubuntu_start` ci-dessous:
+
+   .. figure:: resources/img/docker_ok.gif
+      :name: fig_docker_ubuntu_start
+      :align: center
+
+      Interface graphique utilisant un navigateur pour interagir avec le docker  yguel/mujoco-desktop:v1.0 sous ubuntu avec noVNC.
+
+#. Dans cette dernière fenêtre, vous pouvez maintenant ouvrir un terminal et exécuter les vérifications suivantes.
 
 Assurez-vous que vous utilisez le navigateur web du docker (celui ouvert avec l'URL `http://localhost:6080 <http://localhost:6080>`_) pour faire les vérifications suivantes (cela sera beaucoup plus pratique pour copier/coller les commandes et télécharger les fichiers au bon endroit).
-Dans ce navigateur ouvrez cette page: 'https://yguel.github.io/apprentissage_par_renforcement_et_simulation/l01_introduction_with_mujoco/l01s01_installation_mujoco.html#premiere-verification-de-l-installation'.
+Le navigateur devrait normalement être ouvert à la page suivante: 'https://yguel.github.io/apprentissage_par_renforcement_et_simulation/l01_introduction_with_mujoco/l01s01_installation_mujoco.html#premiere-verification-de-l-installation'.
 
-Pour faire une première vérification que l'installation de MuJoCo s'est bien passée, télécharger le fichier :download:`test_mujoco_gl.py <resources/mujoco_python/test_install/test_mujoco_gl.py>` et placez le dans le dossier `~/rl/mujoco/workspace/examples`.
+Pour faire une première vérification que l'installation de MuJoCo s'est bien passée, télécharger le fichier :download:`test_mujoco_gl.py <resources/mujoco_python/test_install/test_mujoco_gl.py>`.
 
-vous pouvez exécuter la commande suivante dans un terminal:
+vous pouvez exécuter la commande suivante dans un terminal qui place le fichier dans le dossier `~/rl/mujoco/workspace/examples` et l'exécute:
 
 .. code-block:: bash
 
-   cd ~/workspace/examples && python3 test_mujoco_gl.py
+   mv ~/Downloads/test_mujoco_gl.py ~/workspace/examples/ && cd ~/workspace/examples && python3 test_mujoco_gl.py
--- doc/sphinx/source/l01_introduction_with_mujoco/resources/scripts/start.sh
+++ doc/sphinx/source/l01_introduction_with_mujoco/resources/scripts/start.sh
@@ -7,6 +7,10 @@ set -e
 
 # Parse command line arguments
 USE_LOCAL_ONLY=false
+SMALL_RAM_MODE=false
+CUSTOM_RAM_VALUE=""
+VNC_RESOLUTION="1920x1080"
+VNC_QUALITY="high"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -14,16 +18,69 @@ while [[ $# -gt 0 ]]; do
             USE_LOCAL_ONLY=true
             shift
             ;;
+        --small_ram)
+            SMALL_RAM_MODE=true
+            shift
+            ;;
+        --ram)
+            if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+                CUSTOM_RAM_VALUE="$2"
+                shift 2
+            else
+                echo "Error: --ram requires a value (e.g., --ram 1g)"
+                exit 1
+            fi
+            ;;
+        --resolution)
+            if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+                VNC_RESOLUTION="$2"
+                shift 2
+            else
+                echo "Error: --resolution requires a value (e.g., --resolution 1920x1080)"
+                exit 1
+            fi
+            ;;
+        --quality)
+            if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+                VNC_QUALITY="$2"
+                shift 2
+            else
+                echo "Error: --quality requires a value (high, medium, low)"
+                exit 1
+            fi
+            ;;
         -h|--help)
             echo "Usage: $0 [OPTIONS]"
             echo ""
             echo "Options:"
-            echo "  --local    Use local Docker image only, skip remote version check"
-            echo "  -h, --help Show this help message"
+            echo "  --local            Use local Docker image only, skip remote version check"
+            echo "  --small_ram        Use conservative memory settings: min(2GB, 50% RAM)"
+            echo "  --ram SIZE         Use specific memory amount (e.g., --ram 1g, --ram 512m)"
+            echo "  --resolution WxH   Set VNC resolution (default: 1920x1080)"
+            echo "  --quality LEVEL    Set VNC quality: high, medium, low (default: high)"
+            echo "  -h, --help         Show this help message"
+            echo ""
+            echo "Memory allocation:"
+            echo "  Default: min(4GB, 50% RAM)"
+            echo "  --small_ram: min(2GB, 50% RAM)"
+            echo "  --ram SIZE: exact value specified"
+            echo ""
+            echo "VNC Display:"
+            echo "  Default: 1920x1080 @ 24-bit color depth, 96 DPI"
+            echo "  High quality: 24-bit color, standard DPI"
+            echo "  Medium quality: 16-bit color, standard DPI"
+            echo "  Low quality: 8-bit color, low DPI"
+            echo ""
+            echo "Workspace management:"
+            echo "  Persistent: \$HOME/rl/mujoco (if writable)"
+            echo "  Temporary: /tmp/rl/mujoco (auto-backup on exit)"
             echo ""
             echo "Examples:"
-            echo "  $0           # Normal mode (check remote for updates)"
-            echo "  $0 --local  # Local mode (use local image only)"
+            echo "  $0                              # Normal mode: Full HD, high quality"
+            echo "  $0 --resolution 1440x900       # Custom resolution"
+            echo "  $0 --quality medium --small_ram # Medium quality, low memory"
+            echo "  $0 --resolution 2560x1440      # 1440p resolution"
+            echo "  $0 --ram 512m --quality low    # Low resource usage"
             exit 0
             ;;
         *)
@@ -67,18 +124,211 @@ detect_gpu_support() {
     return 1
 }
 
+# Function to install backup script from GitHub
+install_backup_script() {
+    local script_dir="$HOME/.local/bin/rl"
+    local script_path="$script_dir/save_rl_env.sh"
+    local github_url="https://raw.githubusercontent.com/yguel/docker_mujoco_rl_101/main/save_rl_env.sh"
+    # Human-readable logs go to stderr; function prints ONLY the installed path on stdout
+    >&2 echo "🔧 Installing backup script from GitHub..."
+
+    # Create directory
+    mkdir -p "$script_dir"
+
+    # Download latest version from GitHub
+    if command -v curl >/dev/null 2>&1; then
+        if curl -fsSL "$github_url" -o "$script_path" >/dev/null 2>&1; then
+            chmod +x "$script_path"
+            >&2 echo "✅ Backup script installed: $script_path"
+            printf '%s\n' "$script_path"
+            return 0
+        else
+            >&2 echo "❌ Failed to download backup script with curl"
+        fi
+    elif command -v wget >/dev/null 2>&1; then
+        if wget -q "$github_url" -O "$script_path" >/dev/null 2>&1; then
+            chmod +x "$script_path"
+            >&2 echo "✅ Backup script installed: $script_path"
+            printf '%s\n' "$script_path"
+            return 0
+        else
+            >&2 echo "❌ Failed to download backup script with wget"
+        fi
+    else
+        >&2 echo "❌ Neither curl nor wget available for downloading backup script"
+    fi
+
+    return 1
+}
+
+# Function to detect volume binding capability and determine workspace path
+detect_workspace_path() {
+    local home_path="$HOME/rl/mujoco"
+    local tmp_path="/tmp/rl/mujoco"
+    
+    # Create both directories first
+    mkdir -p "$home_path/workspace"/{examples,models,notebooks} 2>/dev/null || true
+    mkdir -p "$tmp_path/workspace"/{examples,models,notebooks}
+    
+    # Test actual Docker volume binding capability
+    >&2 echo "🧪 Testing Docker volume binding capability..."
+
+    # Create a test file in home path
+    local test_file="$home_path/.docker_bind_test"
+    if ! echo "test" > "$test_file" 2>/dev/null; then
+        >&2 echo "⚠️  Cannot write to $home_path, using temporary workspace: $tmp_path"
+        printf '%s|TEMP\n' "$tmp_path"
+        return 0
+    fi
+
+    # Try to bind mount and check if Docker daemon can access it
+    if timeout 10 docker run --rm -v "$home_path:/.test_mount" alpine:latest cat /.test_mount/.docker_bind_test >/dev/null 2>&1; then
+        rm -f "$test_file" 2>/dev/null || true
+        >&2 echo "✅ Docker can bind to home directory: $home_path"
+        printf '%s|PERSIST\n' "$home_path"
+        return 0
+    else
+        rm -f "$test_file" 2>/dev/null || true
+        >&2 echo "⚠️  Docker daemon cannot bind to $home_path, using temporary workspace: $tmp_path"
+        printf '%s|TEMP\n' "$tmp_path"
+        return 0
+    fi
+}
+
+# Detect workspace path and binding capability
+echo ""
+echo "📁 Detecting workspace configuration..."
+WORKSPACE_DETECT_RESULT=$(detect_workspace_path)
+WORKSPACE_PATH="${WORKSPACE_DETECT_RESULT%%|*}"
+WORKSPACE_MODE="${WORKSPACE_DETECT_RESULT##*|}"
+
+# Define backup target directory
+BACKUP_TARGET_DIR="$HOME/rl/docker_snapshots/mujoco_snapshots"
+
+if [ "$WORKSPACE_MODE" = "TEMP" ]; then
+    USE_TEMP_WORKSPACE="true"
+    HOST_WORKSPACE_INFO="Temporary: /tmp/rl/mujoco (will be backed up on exit)"
+    BACKUP_SCRIPT_PATH=$(install_backup_script)
+    if [ $? -eq 0 ]; then
+        echo "🔧 Backup script ready at: $BACKUP_SCRIPT_PATH"
+        # Ensure backup target directory exists and is writable
+        if [ ! -d "$BACKUP_TARGET_DIR" ]; then
+            echo "ℹ️  Backup directory $BACKUP_TARGET_DIR does not exist. Creating it..."
+            if ! mkdir -p "$BACKUP_TARGET_DIR"; then
+                echo "❌ Failed to create backup directory $BACKUP_TARGET_DIR."
+                exit 1
+            fi
+            chmod 700 "$BACKUP_TARGET_DIR" 2>/dev/null || true
+        fi
+        if ! touch "$BACKUP_TARGET_DIR/.backup_test" 2>/dev/null; then
+            echo "❌ Backup directory $BACKUP_TARGET_DIR is not writable. Please fix permissions."
+            exit 1
+        fi
+        rm -f "$BACKUP_TARGET_DIR/.backup_test"
+    else
+        echo "⚠️  Warning: Could not install backup script from GitHub"
+        echo "   Backup on exit will be skipped"
+        BACKUP_SCRIPT_PATH=""
+    fi
+else
+    USE_TEMP_WORKSPACE="false"
+    HOST_WORKSPACE_INFO="Persistent: $WORKSPACE_PATH"
+    BACKUP_SCRIPT_PATH=""
+fi
+
+# Create workspace structure
+echo "📁 Setting up workspace structure..."
+mkdir -p "$WORKSPACE_PATH/workspace"/{notebooks,examples,models}
+
+# Function to calculate memory settings
+calculate_memory_settings() {
+    # If --ram has a value, use it directly
+    if [ -n "$CUSTOM_RAM_VALUE" ]; then
+        >&2 echo "🧠 Custom RAM: ${CUSTOM_RAM_VALUE} (user specified)"
+        printf '%s\n' "$CUSTOM_RAM_VALUE"
+        return
+    fi
+    
+    # Get total RAM in GB (from /proc/meminfo)
+    local total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+    local total_ram_gb=$((total_ram_kb / 1024 / 1024))
+    
+    local shm_size_gb
+    
+    if [ "$SMALL_RAM_MODE" = true ]; then
+        # Small RAM mode: max(2GB, 50% RAM)
+        local fifty_percent=$((total_ram_gb / 2))
+        if [ $fifty_percent -gt 2 ]; then
+            shm_size_gb=2
+        else
+            shm_size_gb=$fifty_percent
+        fi
+        >&2 echo "🧠 Small RAM mode: ${shm_size_gb}GB (min of 2GB or 50% of ${total_ram_gb}GB)"
+    else
+        # Normal mode: min(4GB, 50% RAM)
+        local fifty_percent=$((total_ram_gb / 2))
+        if [ $fifty_percent -gt 4 ]; then
+            shm_size_gb=4
+        else
+            shm_size_gb=$fifty_percent
+        fi
+        >&2 echo "🧠 Memory: ${shm_size_gb}GB (min of 4GB or 50% of ${total_ram_gb}GB RAM)"
+    fi
+    
+    printf '%sg\n' "$shm_size_gb"
+}
+
 # Function to build Docker run command with smart OpenGL
 build_docker_command() {
-    local cmd="docker run --rm --name $CONTAINER_NAME"
+    local gpu_support=$1
+    local workspace_path=$2
+    
+    # Calculate memory settings
+    local shm_size=$(calculate_memory_settings)
+    
+    # Base command with memory settings
+    cmd="docker run -it --rm --name $CONTAINER_NAME --shm-size=${shm_size}"
     
     # Basic ports and volumes
     cmd="$cmd -p 6080:6080"        # noVNC desktop
     cmd="$cmd -p 8888:8888"        # Jupyter notebook
-    cmd="$cmd -v /tmp/rl/mujoco/workspace:/home/student/workspace"
+    cmd="$cmd -v ${workspace_path}/workspace:/home/student/workspace"
     
     # Pass host user UID and GID for proper file permissions
     cmd="$cmd -e HOST_UID=$(id -u)"
     cmd="$cmd -e HOST_GID=$(id -g)"
+    
+    # Pass workspace information to entrypoint
+    cmd="$cmd -e HOST_WORKSPACE_INFO=\"$HOST_WORKSPACE_INFO\""
+    cmd="$cmd -e USE_TEMP_WORKSPACE=\"$USE_TEMP_WORKSPACE\""
+    cmd="$cmd -e BACKUP_TARGET_DIR=\"$BACKUP_TARGET_DIR\""
+    
+    # VNC settings for better resolution and quality
+    case $VNC_QUALITY in
+        high)
+            VNC_DEPTH=24
+            VNC_DPI=96
+            ;;
+        medium)
+            VNC_DEPTH=16
+            VNC_DPI=96
+            ;;
+        low)
+            VNC_DEPTH=8
+            VNC_DPI=72
+            ;;
+        *)
+            echo "⚠️  Unknown quality '$VNC_QUALITY', using high quality"
+            VNC_DEPTH=24
+            VNC_DPI=96
+            ;;
+    esac
+    
+    cmd="$cmd -e VNC_RESOLUTION=$VNC_RESOLUTION"   # User-specified or default resolution
+    cmd="$cmd -e VNC_DEPTH=$VNC_DEPTH"             # Quality-based color depth
+    cmd="$cmd -e VNC_DPI=$VNC_DPI"                 # Quality-based DPI
+    cmd="$cmd -e NOVNC_PORT=6080"                  # noVNC port
+    cmd="$cmd -e VNC_PORT=5901"                    # VNC direct port
     
     # GPU support if available (check silently)
     if [ -d "/dev/dri" ] && [ "$(ls -A /dev/dri 2>/dev/null)" ] || (command -v nvidia-smi >/dev/null 2>&1 && nvidia-smi >/dev/null 2>&1); then
@@ -122,9 +372,9 @@ cleanup() {
 # Set trap for Ctrl-C
 trap cleanup SIGINT SIGTERM
 
-# Create workspace structure
-echo "📁 Setting up workspace..."
-mkdir -p /tmp/rl/mujoco/workspace/{notebooks,examples,models}
+# Create workspace structure (WORKSPACE_PATH already detected earlier)
+echo "📁 Setting up workspace structure..."
+mkdir -p "$WORKSPACE_PATH/workspace"/{notebooks,examples,models}
 
 # Stop any existing container
 if docker ps -a | grep -q "$CONTAINER_NAME"; then
@@ -195,8 +445,19 @@ else
     echo "   → Will use software rendering (OSMesa)"
 fi
 
+# Show VNC configuration
+echo ""
+echo "🖥️  VNC Display Configuration:"
+echo "   Resolution: $VNC_RESOLUTION"
+echo "   Quality: $VNC_QUALITY"
+case $VNC_QUALITY in
+    high)   echo "   → 24-bit color, 96 DPI (best quality)" ;;
+    medium) echo "   → 16-bit color, 96 DPI (balanced)" ;;
+    low)    echo "   → 8-bit color, 72 DPI (performance)" ;;
+esac
+
 # Build and display the command
-DOCKER_CMD=$(build_docker_command)
+DOCKER_CMD=$(build_docker_command true "$WORKSPACE_PATH")
 echo $DOCKER_CMD
 
 echo ""
@@ -208,11 +469,32 @@ echo "   🌐 Access will be available at:"
 echo "      Desktop: http://localhost:6080"
 echo "      Jupyter: http://localhost:8888"
 echo "   📁 Workspace folder:"
-echo "      Local:     /tmp/rl/mujoco/workspace"
+echo "      Host: $HOST_WORKSPACE_INFO"
 echo "      Container: /home/student/workspace"
+if [ "$USE_TEMP_WORKSPACE" = "true" ]; then
+    echo "      ⚠️  Using temporary workspace - will be backed up on exit to: $BACKUP_TARGET_DIR"
+fi
 echo ""
 echo "⏳ Starting services (this may take 30 seconds)..."
 echo "=============================================="
+
+# Function for cleanup on exit
+cleanup_on_exit() {
+    echo ""
+    echo "🛑 Stopping container..."
+    docker stop "$CONTAINER_NAME" >/dev/null 2>&1 || true
+    
+    # If using temp workspace, create backup
+    if [ "$USE_TEMP_WORKSPACE" = "true" ] && [ -n "$BACKUP_SCRIPT_PATH" ] && [ -f "$BACKUP_SCRIPT_PATH" ]; then
+        echo "💾 Creating backup of temporary workspace..."
+        "$BACKUP_SCRIPT_PATH" "$BACKUP_TARGET_DIR"
+    fi
+    
+    echo "✅ Environment stopped"
+}
+
+# Set trap for cleanup
+trap cleanup_on_exit SIGINT SIGTERM EXIT
 
 # Run the container in foreground
 eval $DOCKER_CMD
